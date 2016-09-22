281b4442e9dc688762bedf427c0123d00c81fb71
Ted <theddy_lopes@hotmail.com>
2016-09-22 20:10:47 -0300

    tentando instalar alguns componentes do bower

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var watch = require('gulp-watch');
+
+
+//task para o sass
+gulp.task('sass', function(){
+    return sass('sass/**/*.sass').pipe(gulp.dest('css'))
+});
+
+//task para o watch
+gulp.task('watch', function(){
+    gulp.watch('sass/**/*.sass', ['sass'])
+});
+
+//task gulp default
+/*gulp.task('default', function(){
+    console.log('Ola...');
+});*/
+
+/*Obs: caso eu queira digitar somente gulp no meu terminal 
+do Node para rodar todas as tasks de uma só vez, devo utilizar 
+a task default do gulp com um array como parâmetro contendo as
+chamadas das task/plugins utilizados. 
+
+Exemplo:
+*/
+//task gulp Geral
+gulp.task('default', ['sass', 'watch']); //Essa task chama as funções do sass e do watch conjuntamente
+  
+
